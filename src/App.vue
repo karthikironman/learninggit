2a354cc5fa732f774fc3366282baecf0c64a5854
karthikironman <karthik.selvaraj@diatoz.com>
2019-08-01 16:24:07 +0530

    initial commit uploading the src

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+
+  <input v-model="name">  
+  
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+   
+  },
+  data(){
+    return{
+      decider:true,
+      scpt:"<h1>this is a heading!</h1>",
+     d : new Date(),
+     info:'',
+     loading: true,
+     errored: false,
+     name:''
+    }
+  },
+ mounted() {
+   if(localStorage.name){
+     this.name = localStorage.name;
+   }
+ },
+ watch:{
+   name(newName){
+     localStorage.name = newName;
+   }
+ }
+
+}
+</script>
+
+<style>
+.one{
+  color:red;
+  
+}
+</style>
